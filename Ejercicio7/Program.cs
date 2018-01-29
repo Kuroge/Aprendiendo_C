7c60ead4f33b00d18efaeba1ab16793bbe50d852
Adrián García Riera <i7610@softgal.com>
2018-01-29 11:01:00 +0100

    29/01/18

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClasClientesBase cliente = new ClasClientesBase(0, "Adrián García Riera", "001", "HEHM", 1, "Adrián García Riera");
+
+            Console.WriteLine(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+
+        }
+    }
+}
